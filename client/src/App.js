07d18b37ef2b231d5ad3453ed99d5060f982b733
emritcey <emma.ritcey@spr.com>
2020-05-06 21:59:49 -0500

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,48 @@
-import React, {useEffect} from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
 
-    useEffect(() => {
-        callBackendAPI().then(body => {
-            console.log(body);
-        })
-    });
-
-    const callBackendAPI = async () => {
-        const response = await fetch('/express_backend');
-        const body = await response.json();
-        return body;
+    state = {
+        data: null
     };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidMount() {
+        this.callBackendAPI();
+    }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    callBackendAPI = async () => {
+        try {
+            const response = await fetch('/express_backend');
+            const body = await response.json();
+            this.setState({ data: body.express });
+        } catch(err) {
+            throw Error(err)
+        }
+        return;
+    };
+
+  render () {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+  }
 }
 
 export default App;
