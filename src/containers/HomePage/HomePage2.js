38975f8e29cbf3777b56b338386fd1d10172f0e5
Chris R <45723002+critcey68@users.noreply.github.com>
2020-05-24 20:01:13 -0400

    added photos and grid to home page

--- src/containers/HomePage/HomePage2.js
+++ src/containers/HomePage/HomePage2.js
@@ -1,10 +1,75 @@
 import React from "react";
 import  "../../assets/css/cr.css";
+import  "../../assets/css/HomePage.css"
+import lake5x5 from '../../assets/images/5x5_lake.jpg';
+import mountain3x5 from '../../assets/images/3x5.jpg';
+import river5x3 from '../../assets/images/5x3.jpg';
 
 class HomePage2 extends React.Component {
 render() {
 return (
-  <h1> Home Page 2 </h1>
+
+  <div class="wrapper">
+    <div class="i1">
+      <figure>
+        <img  className = 'imgFrontPage' src={lake5x5}  alt="no image"/>
+        <figcaption><i>Purple Lake Sunset</i></figcaption>
+      </figure>
+    </div>
+    <div class="i2">
+      <figure>
+        <img  className = 'imgFrontPage' src={river5x3}  alt="no image"/>
+        <figcaption><i>Evolution Valley Ahead...</i></figcaption>
+      </figure>
+    </div>
+    <div class="i3">
+      <figure>
+        <img  className = 'imgFrontPage' src={mountain3x5}  alt="no image"/>
+        <figcaption><i>Minarets</i></figcaption>
+      </figure></div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>10</div>
+    <div>11</div>
+    <div>12</div>
+    <div>13</div>
+    <div>14</div>
+    <div>15</div>
+    <div>16</div>
+    <div>17</div>
+    <div>18</div>
+    <div>19</div>
+    <div>20</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>21</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+    <div>41</div>
+
+
+  </div>
+
+
 );
 }}
 
